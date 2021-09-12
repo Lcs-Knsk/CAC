810bcd154ec266567124d068e5e03cb0def8095c
Lcs-Knsk <84292789+Lcs-Knsk@users.noreply.github.com>
2021-09-12 17:04:04 -0400

    Total project so far

---
+++ Character.js
@@ -0,0 +1,110 @@
+class Character{
+    constructor(XPos, YPos){
+        this.Main = {
+            X: XPos,
+            Y: YPos,
+
+            Height: 64,
+            Width: 48,
+
+            Speed: 3,
+        }
+
+        //contains all of the movement variables of the character
+        this.Move = {
+            Left: false,
+            Right: false,
+            Up: false,
+            Down: false
+        }
+
+        //contains all of the animation variables
+        this.Anim = {
+            FrameRow: 0,
+            PlayerImage: new Image,
+
+            FrameWidth: 48,
+            FrameHeight: 63,
+
+            FrameNumber: 0,
+            HighFrameNumber: 3,
+            Animate: true
+        }
+
+        this.Anim.PlayerImage.src = "SpritesAndStuff/NewChar.png";
+
+    }
+
+    Loop(){
+        //Movement
+        this.MoveChar();
+
+        //Draw
+        this.Animation();
+
+        //Collisions
+
+    }
+
+
+
+    //Moves the player
+    MoveChar(){
+        if(this.Move.Down){
+            this.Main.Y += this.Main.Speed;
+        }
+        else if(this.Move.Left){
+            this.Main.X -= this.Main.Speed;
+        }
+        
+        else if(this.Move.Right){
+            this.Main.X += this.Main.Speed;
+        }
+        else if(this.Move.Up){
+            this.Main.Y -= this.Main.Speed;
+        }
+
+
+    }
+
+    //draws the player
+    Animation(){
+        //sets what row the animaiton should be playing on
+        this.DetectAnimRow();
+
+        if(this.Anim.Animate){
+            ctx.drawImage(this.Anim.PlayerImage, (this.Anim.FrameWidth+16)*this.Anim.FrameNumber, this.Anim.FrameHeight*this.Anim.FrameRow,
+            this.Anim.FrameWidth, this.Anim.FrameHeight, this.Main.X, this.Main.Y, this.Anim.FrameWidth, this.Anim.FrameHeight);
+        }
+        else{
+            ctx.drawImage(this.Anim.PlayerImage, (this.Anim.FrameWidth+16)*1, this.Anim.FrameHeight*0,
+                this.Anim.FrameWidth, this.Anim.FrameHeight, this.Main.X, this.Main.Y, this.Anim.FrameWidth, this.Anim.FrameHeight);
+        }
+
+
+    }
+
+    DetectAnimRow(){
+        if(this.Move.Down){
+            this.Anim.FrameRow = 0;
+            this.Anim.Animate = true;
+        }
+        else if(this.Move.Left){
+            this.Anim.FrameRow = 1;
+            this.Anim.Animate = true;
+        }
+        else if(this.Move.Right){
+            this.Anim.FrameRow = 2;
+            this.Anim.Animate = true;
+        }
+        else if(this.Move.Up){
+            this.Anim.FrameRow = 3;
+            this.Anim.Animate = true;
+        }
+        else{
+            //makes the animation stop||plays idle animation(not really an animation though)
+            this.Anim.Animate = false;
+        }
+    }
+
+}
