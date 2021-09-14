fa8fc102e7fb0f89167bb8de91de1627d60c333f
Lcs-Knsk <84292789+Lcs-Knsk@users.noreply.github.com>
2021-09-14 12:57:41 -0400

    Changed the aspect ratio of the map.

--- CAC-main/Map.js
+++ CAC-main/Map.js
@@ -1,16 +1,17 @@
 class IslandMap{
     constructor(){
-        this.MapX = 400;
-        this.MapY = 400;
+        //These two vars are the coords for the top left of the map generation
+        this.MapX = 0;
+        this.MapY = 0;
 
-        this.MapWidth = 400;
-        this.MapHeight = 400;
+        this.ScreenWidth = 600;
+        this.ScreenHeight = 400;
 
         this.mapThing = new Image;
         this.mapThing.src = "SpritesAndStuff/cacTrueMap.png"     
     }
 
     DrawMap(){
-        ctx.drawImage(this.mapThing, this.MapX, this.MapY, this.MapWidth, this.MapHeight, 0, 0, 800, 800);
+        ctx.drawImage(this.mapThing, this.MapX, this.MapY, this.ScreenWidth, this.ScreenHeight, 0, 0, 1000, 800);
     }
 }
